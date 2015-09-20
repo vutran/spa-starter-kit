8197c362709fc156039857db19df664e4c4f8b44
Vu Tran <vu@vu-tran.com>
2015-09-19 20:43:22 -0700

    removed address from server console.log

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use('/dist', express.static(__dirname + '/dist'));
 // Set the port
 var port = process.env.PORT || 4000
 
-// Listen to port 80
+// Listen to the port
 var server = app.listen(port, function(err, result) {
     if (err) {
         console.log(err);
     }
-  console.log('Running on http://%s:%s', 'localhost', server.address().port);
+  console.log('Running on port %s', port);
 });
