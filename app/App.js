d0b7e09b2c6d6528bf44c74254bf64a2c8bc6c87
Vu Tran <vu@vu-tran.com>
2015-09-22 01:58:56 -0700

    updated default page

--- app/App.js
+++ app/App.js
@@ -8,6 +8,13 @@ export default class App extends React.Component {
      * Renders the component
      */
     render() {
-        return <h1>Congratulations!</h1>
+        return (
+            <div className="intro">
+                <h1>Congratulations!</h1>
+                <p>You are now ready to begin your new ReactJS application.</p>
+                <p>To get started, visit the <a href="http://facebook.github.io/react/" target="_blank">ReactJS</a> website.</p>
+                <h2 className="footnote">Got a feedback or suggestion to this starter kit? <a href="https://github.com/vutran/spa-starter-kit/issues" target="_blank">Post on GitHub.</a></h2>
+            </div>
+        );
     }
 }
