398b3962ab6408d45b56a37231e7ea78467dbbb4
Vu Tran <vu@vu-tran.com>
2015-09-26 11:16:11 -0700

    refactored css modules

--- app/App.js
+++ app/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './styles/global.scss';
 
 /**
  * <App />
@@ -9,11 +10,11 @@ export default class App extends React.Component {
      */
     render() {
         return (
-            <div className="intro">
+            <div className={styles.intro}>
                 <h1>Congratulations!</h1>
                 <p>You are now ready to begin your new ReactJS application.</p>
                 <p>To get started, visit the <a href="http://facebook.github.io/react/" target="_blank">ReactJS</a> website.</p>
-                <h2 className="footnote">Got a feedback or suggestion to this starter kit? <a href="https://github.com/vutran/spa-starter-kit/issues" target="_blank">Post on GitHub.</a></h2>
+                <h2 className={styles.footnote}>Got a feedback or suggestion to this starter kit? <a href="https://github.com/vutran/spa-starter-kit/issues" target="_blank">Post on GitHub.</a></h2>
             </div>
         );
     }
